ab1f0836b19792b36db47e4eca790660a384bbed
Michael Bowen <mikee.bowen@gmail.com>
2019-05-04 10:37:37 -0700

    add method to create data in a file

--- index.js
+++ index.js
@@ -3,18 +3,25 @@
 const { join } = require('path');
 const yargs = require('yargs');
 const createFromFile = require('./lib/createFromFile');
+const createFromCode = require('./lib/createFromCode');
 
 // yargs.array('location');
 // Object.keys(faker).forEach(key => {
 //   yargs.array(key);
 // });
 
-const argv = yargs.argv;
+if (require.main === module) {
+  const argv = yargs.argv;
 
-if (typeof argv.file === 'string' && argv.file != undefined) {
-  const readPathArr = argv.file ? argv.file.split('/') : ['testData'];
-  readPathArr.unshift(process.cwd());
-  const readFileName = readPathArr[readPathArr.length - 1].endsWith('.json') ? readPathArr.pop() : `${readPathArr.pop()}.json`;
-  const readPath = `${join(...[...readPathArr, readFileName])}`;
-  createFromFile(readPath);
+  if (typeof argv.file === 'string' && argv.file != undefined) {
+    const readPathArr = argv.file ? argv.file.split('/') : ['testData'];
+    readPathArr.unshift(process.cwd());
+    const readFileName = readPathArr[readPathArr.length - 1].endsWith('.json')
+      ? readPathArr.pop()
+      : `${readPathArr.pop()}.json`;
+    const readPath = `${join(...[...readPathArr, readFileName])}`;
+    createFromFile(readPath);
+  }
+} else {
+  module.exports = createFromCode;
 }
