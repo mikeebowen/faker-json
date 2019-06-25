01e4a5efc1a0709e4c20f5f726c30ca9eae860c8
Michael Bowen <mikee.bowen@gmail.com>
2019-06-24 18:57:12 -0700

    add index.js that somehow got removed...

---
+++ bin/index.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+'use strict';
+const { join } = require('path');
+const yargs = require('yargs');
+const createFromFile = require('../lib/createFromFile');
+const createData = require('../lib/createData');
+
+if (require.main === module) {
+  const argv = yargs.argv;
+
+  if (typeof argv.file === 'string' && argv.file != undefined) {
+    const readPathArr = argv.file ? argv.file.split('/') : ['testData'];
+    readPathArr.unshift(process.cwd());
+    const readFileName = readPathArr[readPathArr.length - 1].endsWith('.json')
+      ? readPathArr.pop()
+      : `${readPathArr.pop()}.json`;
+    const readPath = `${join(...[...readPathArr, readFileName])}`;
+    createFromFile(readPath);
+  }
+} else {
+  module.exports = createData;
+}
